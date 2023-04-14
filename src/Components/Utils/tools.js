165ac3d2f72a4e1dbe8b0cb329c74c3b8d7c2d9f
lzx8035 <lzx8035@gmail.com>
2023-04-13 23:54:32 -0700

    add logo and footer

---
+++ src/Components/Utils/tools.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import MCLOGO from "../../Resources/images/logos/manchester_city_logo.png";
+
+export const CityLogo = (props) => {
+  const template = (
+    <div
+      className="img_cover"
+      style={{
+        width: props.width,
+        height: props.height,
+        background: `url(${MCLOGO}) no-repeat`,
+      }}
+    ></div>
+  );
+
+  if (props.link) {
+    return (
+      <Link className="link_logo" to={props.linkTo}>
+        {template}
+      </Link>
+    );
+  } else {
+    return template;
+  }
+};
