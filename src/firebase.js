a61e0fa952fdf02babbbf2a4af77f09eb2511aea
lzx8035 <lzx8035@gmail.com>
2023-04-28 11:03:43 -0700

    add data to firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { addDoc, getDocs, collection } from "firebase/firestore";
+
+import { cityDb } from "./temp/m-city-export";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCh5la64Ka3QMR3dQL860uzFhp6vg80gv8",
   authDomain: "m-city-c6612.firebaseapp.com",
@@ -19,6 +22,84 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(firebase);
+const matchesCollection = collection(db, "matches");
+const playersCollection = collection(db, "players");
+const positionsCollection = collection(db, "positions");
+const promotionsCollection = collection(db, "promotions");
+const teamsCollection = collection(db, "teams");
+
+/*
+////////////////////////////////////////////////////////////////////////////////////
+// 1) ADD MATCHES TO DATABASE //////////////////////////////////////////////////////
+async function addMatches(matches) {
+  for (const item of matches) {
+    try {
+      await addDoc(matchesCollection, item);
+    } catch (e) {
+      console.error("Error adding: ", e);
+    }
+  }
+}
+addMatches(cityDb.matches);
+
+// 2) ADD PLAYERS TO DATABASE //////////////////////////////////////////////////////
+async function addPlayers(matches) {
+  for (const item of matches) {
+    try {
+      await addDoc(playersCollection, item);
+    } catch (e) {
+      console.error("Error adding: ", e);
+    }
+  }
+}
+addPlayers(cityDb.players);
+
+// 3) ADD Positions TO DATABASE ////////////////////////////////////////////////////
+async function addPositions(matches) {
+  for (const item of matches) {
+    try {
+      await addDoc(positionsCollection, item);
+    } catch (e) {
+      console.error("Error adding: ", e);
+    }
+  }
+}
+addPositions(cityDb.positions);
+
+// 4) ADD Promotions TO DATABASE ////////////////////////////////////////////////////
+async function addPromotions(matches) {
+  for (const item of matches) {
+    try {
+      await addDoc(promotionsCollection, item);
+    } catch (e) {
+      console.error("Error adding: ", e);
+    }
+  }
+}
+addPromotions(cityDb.promotions);
+
+// 5) ADD Teams TO DATABASE /////////////////////////////////////////////////////////
+async function addTeams(matches) {
+  for (const item of matches) {
+    try {
+      await addDoc(teamsCollection, item);
+    } catch (e) {
+      console.error("Error adding: ", e);
+    }
+  }
+}
+addTeams(cityDb.teams);
+////////////////////////////////////////////////////////////////////////////////////
+*/
 
 export default firebase;
+export {
+  matchesCollection,
+  playersCollection,
+  positionsCollection,
+  promotionsCollection,
+  teamsCollection,
+};
